b6f22beace1f9f6e45b5cb01640ee1b1e772bc69
victor-say <new.victor.sy@gmail.com>
2025-11-02 12:36:45 -0300

    feat: crud books ->  method crate

--- app/Http/Controllers/Book/BookController.php
+++ app/Http/Controllers/Book/BookController.php
@@ -8,9 +8,7 @@ use App\Models\Book;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
         $books = Book::paginate(20);
@@ -18,49 +16,37 @@ class BookController extends Controller
         return view('book.index', compact('books'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+
     public function create()
     {
-        //
+        return view('book.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
+    
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(string $id)
     {
         //
